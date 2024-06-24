28c42a598f98362cbf9d0ea92cfb7252923ced8e
Vithor Mitsuyoshi <vithor.mitsuyoshi@superfestval.com.br>
2024-06-24 18:05:53 -0300

    refactor: update index of application

--- index.ts
+++ index.ts
@@ -1,4 +1,17 @@
 import "dotenv/config"
 import { start } from "./src/core/UseCase/StartBotUseCase/LayoutBotUseCase"
+import express from "express"
+import route from "./routes"
+
+const app = express()
+app.use(express.json())
+
+const port = 3333;
+
+app.use(route)
 
 start
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
+})
