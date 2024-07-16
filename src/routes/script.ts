701f7b617ff4ccfe569490c9447b171b649f1388
Vithor Mitsuyoshi <vithor.mitsuyoshi@superfestval.com.br>
2024-07-16 17:25:42 -0300

    refactor: add new route on scripts

--- src/routes/script.ts
+++ src/routes/script.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { findScriptController } from "../infra/controller/ScriptController/FindScriptController/index.js";
 import { createScriptController } from "../infra/controller/ScriptController/CreateScriptUseCase/index.js";
 import { findManyScriptController } from "../infra/controller/ScriptController/FindManyScriptController/index.js";
+import { getProductsController } from "../infra/controller/ScriptController/GetProductsUseCase/index.js";
 
 const scriptRouter = Router()
 
@@ -12,6 +13,9 @@ scriptRouter
     )
     .get("/script/findMany", async (req, res) =>
         findManyScriptController.execute(req, res)
+    )
+    .get("/script/products", async (req, res) =>
+        getProductsController.execute(req, res)
     )
     .post("/script/create", async (req, res) =>
         createScriptController.execute(req, res)
