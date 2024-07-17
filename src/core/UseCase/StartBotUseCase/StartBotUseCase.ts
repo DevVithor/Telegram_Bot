72072ceaa92e9c860c21a68bcc4e760855e64c5b
Vithor Mitsuyoshi <vithor.mitsuyoshi@superfestval.com.br>
2024-07-17 15:46:46 -0300

    refactor: change use Case of start bot

--- src/core/UseCase/StartBotUseCase/StartBotUseCase.ts
+++ src/core/UseCase/StartBotUseCase/StartBotUseCase.ts
@@ -1,58 +1,26 @@
+import { ScriptDTO } from "../../../infra/controller/ScriptController/ScriptDTO.js";
 import { bot } from "../../../infra/lib/TelegramBot.js";
 import { deley } from "../../../infra/lib/Timer.js";
-import { BadRequest } from "../../../middleware/BadRequest.js";
-import { FindGroupRepository } from "../../repository/GroupRepository/FindGroupRepository/FindGroupRepository.js";
-import { FindManyScriptRepository } from "../../repository/ScriptRepository/FindManyScriptRepository/FindManyScriptRepository.js";
-
 export class StartBotUseCase {
-    constructor(
-        private findGroupRepository: FindGroupRepository,
-        private findManyScriptRepository: FindManyScriptRepository
-    ) { }
+    constructor() { }
 
-    async execute(groupId: number) {
+    async execute(data: ScriptDTO) {
 
-        const findGroup = await this.findGroupRepository.findGroup(groupId)
-
-        if (!findGroup) {
-
-            throw new BadRequest("Grupo não existe", "O id do grupo não existe, indique um valor valido!")
-        }
-
-        const findScript = await this.findManyScriptRepository.findManyScript(groupId)
-
-        const validity = findGroup.validity
-
-        while (validity) {
-
-            for (let i = 0; findScript.length > i; i++) {
-
-                const script = `
+        const script = `
                 
-${findScript[i].product}
+${data.product}
 
-${findScript[i].description}
+${data.description}
 
-${findScript[i].descont}
+${data.descont}
 
-${findScript[i].link}
+${data.link}
                 `
 
-                await bot.telegram.sendMessage(`${process.env.CHAT_ID}`, script)
+        await bot.telegram.sendMessage(`${process.env.CHAT_ID}`, script)
 
-                await deley(43200000)
+        await deley(10000)
 
-            }
-
-            let data = new Date()
-
-            if (data > findGroup.validity) {
-
-                return
-            }
-
-        }
-        return
     }
 
 }
--- src/core/UseCase/StartBotUseCase/index.ts
+++ src/core/UseCase/StartBotUseCase/index.ts
@@ -1,5 +1,3 @@
-import { findGroupRepository } from "../../repository/GroupRepository/FindGroupRepository/index.js";
-import { findManyScriptRepository } from "../../repository/ScriptRepository/FindManyScriptRepository/index.js";
 import { StartBotUseCase } from "./StartBotUseCase.js";
 
-export const startBotUseCase = new StartBotUseCase(findGroupRepository, findManyScriptRepository)
+export const startBotUseCase = new StartBotUseCase()
