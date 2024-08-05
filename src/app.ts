f809815f08a9db6b4f278d0a476e78f6b683b5ef
Vithor Mitsuyoshi <vithor.mitsuyoshi@superfestval.com.br>
2024-08-05 14:07:40 -0300

    refactor: add new import

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "express-async-errors"
 import express from "express";
+import cors from "cors";
 
 import { errorHandler } from "./middleware/ErrorHandler.js";
 
@@ -7,13 +8,16 @@ import { bot } from "./infra/lib/TelegramBot.js";
 import botRouter from "./routes/bot.js";
 import groupRouter from "./routes/group.js";
 import scriptRouter from "./routes/script.js";
+import userRoute from "./routes/user.js";
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use(groupRouter)
 app.use(scriptRouter)
 app.use(botRouter)
+app.use(userRoute)
 
 bot.launch()
 
