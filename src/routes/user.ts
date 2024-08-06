dd6e94e59619ca51350f3cb49d22ac4e3ab3209f
Vithor Mitsuyoshi <vithor.mitsuyoshi@superfestval.com.br>
2024-08-06 15:03:29 -0300

    refactor: add new routes on users

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import { createUserController } from "../infra/controller/UserController/index.js";
+import { loginUserController } from "../infra/controller/LoginController/index.js";
+import { authController } from "../infra/controller/AuthController/index.js";
+import { AuthMiddleware } from "../middleware/Auth/AuthMiddleware.js";
 
 const userRoute = Router()
 
-userRoute.
-    post("/users/create", (req, res) => {
+userRoute
+    .post("/users/register", (req, res) =>
         createUserController.execute(req, res)
-    })
+    )
+    .post("/users/login", (req, res) =>
+        loginUserController.execute(req, res)
+    )
+    .get("/auth", AuthMiddleware, (req, res) =>
+        authController.execute(req, res)
+    )
 
 export default userRoute
